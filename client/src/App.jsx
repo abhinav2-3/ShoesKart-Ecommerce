de8a96696f9b235d399d8e67665424fa1c574f57
Abhinav <abhinavgtechnical@gmail.com>
2024-11-18 10:28:15 +0530

    Optimised the App and added Vercel Analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,28 @@
 import "./styles/App.scss";
-import { useEffect } from "react";
+import { useEffect, Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./screens/Home";
+import { Toaster, toast } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import About from "./screens/About";
-import SingleProduct from "./screens/SingleProduct";
-import Signup from "./screens/Signup";
-import Login from "./screens/Login";
-import Products from "./screens/Products";
-import Cart from "./screens/Cart";
-import ErrorPage from "./components/ErrorPage";
-import Contacts from "./screens/Contacts";
-import MyOrder from "./screens/MyOrder";
-import UserProfile from "./screens/UserProfile";
-import { Toaster, toast } from "react-hot-toast";
-import Admin from "./screens/AddProduct";
+import Loader from "./components/Loader";
+
+const Home = lazy(() => import("./screens/Home"));
+const About = lazy(() => import("./screens/About"));
+const SingleProduct = lazy(() => import("./screens/SingleProduct"));
+const Signup = lazy(() => import("./screens/Signup"));
+const Login = lazy(() => import("./screens/Login"));
+const Products = lazy(() => import("./screens/Products"));
+const Cart = lazy(() => import("./screens/Cart"));
+const ErrorPage = lazy(() => import("./components/ErrorPage"));
+const Contacts = lazy(() => import("./screens/Contacts"));
+const MyOrder = lazy(() => import("./screens/MyOrder"));
+const UserProfile = lazy(() => import("./screens/UserProfile"));
+const Admin = lazy(() => import("./screens/AddProduct"));
 
 function App() {
   const notify = () =>
-    toast.loading("Please wait a moment", { duration: 2000 });
+    toast.loading("Please wait a moment", { duration: 1500 });
 
   useEffect(() => {
     notify();
@@ -27,26 +30,28 @@ function App() {
 
   return (
     <Router>
+      <Analytics />
       <Toaster />
       <Header />
-      <Routes>
-        {/* USER ROUTES */}
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contacts />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/myOrders" element={<MyOrder />} />
-        <Route path="/profile" element={<UserProfile />} />
-        <Route path="/singleProduct" element={<SingleProduct />} />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          {/* USER ROUTES */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contacts />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/myOrders" element={<MyOrder />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/singleProduct" element={<SingleProduct />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<ErrorPage />} />
 
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<ErrorPage />} />
-
-        {/* ADMIN ROUTES */}
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
+          {/* ADMIN ROUTES */}
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
